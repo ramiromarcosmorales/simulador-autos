11a49720607ed836bafda6d5de267d711cb222d1
ramiromarcosmorales <ramiro.marcos.ck@gmail.com>
2026-06-20 22:13:25 -0300

    feat: command create user

--- build.sh
+++ build.sh
@@ -6,3 +6,5 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --noinput
 python manage.py migrate
+
+python manage.py shell -c "from django.contrib.auth.models import User; User.objects.filter(username='admin').exists() or User.objects.create_superuser('admin', 'admin@admin.com', 'password123')"
